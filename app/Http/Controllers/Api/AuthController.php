437a66e63ff3f38ad2d52cd78c6799cb89cd337b
Matius Rimbe <matiusrimbe.ptik@gmail.com>
2023-11-19 18:14:28 +0800

    create fungsi to login, signup. logout

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\SignupRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function signup(SignupRequest $request): Response
+    {
+        $data = $request->validated();
+        /** @var \App\Models\User $user */
+        $user = User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => bcrypt($data['password']),
+        ]);
+
+        $token = $user->createToken('main')->plainTextToken;
+
+        return response(compact('user', 'token'));
+    }
+
+    public function login(LoginRequest $request): Response
+    {
+        $credentials = $request->validated();
+
+        if (!Auth::attempt($credentials)) {
+            return response([
+                'message' => 'Provided email address or password is incorrect',
+            ], 422);
+        }
+
+        /** @var User $user */
+        $user = Auth::user();
+        $token = $user->createToken('main')->plainTextToken;
+
+        return response(compact('user', 'token'));
+    }
+
+    public function logout(Request $request): Response
+    {
+        /** @var User $user */
+
+        $user = $request->user();
+        $user->currentAccessToken()->delete;
+
+        return response('', 204);
+    }
+}
